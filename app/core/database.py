052c87433a0b59be5102ec95955938210c8b7d0d
Gabriel Corrêa <gabrielcorrreaa@gmail.com>
2025-10-18 23:26:29 -0300

    feat: Migra para PostgreSQL e configura para deploy

--- app/core/database.py
+++ app/core/database.py
@@ -1,14 +1,28 @@
-# app/core/database.py
+
+import os 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./app/financeiro.db"
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app/financeiro.db")
 
 engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
+
+
+if DATABASE_URL.startswith("postgresql"):
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True 
+    )
+else: 
+    engine = create_engine(
+        DATABASE_URL, 
+        connect_args={"check_same_thread": False}
+    )
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
